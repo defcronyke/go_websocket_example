66e097f8fac1d5e711a5497429e1933ab4182f37
Jeremy Carter <Jeremy@JeremyCarter.ca>
2014-10-05 13:56:03 -0400

    added EmitServer() example
    It shows how to emit a message to all connected clients.

--- main.go
+++ main.go
@@ -4,18 +4,70 @@ import (
 	"code.google.com/p/go.net/websocket"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-func EchoServer(ws *websocket.Conn) { // Simplest WebSocket Server
+var num_connections uint64 = 0
+var connections map[uint64]*websocket.Conn
+
+// Simplest WebSocket Server
+func EchoServer(ws *websocket.Conn) {
+
+	conn_num := num_connections // Keep track of all connections.
+	connections[conn_num] = ws
+	num_connections++
+
+	log.Printf("connection id %v opened", conn_num)
+
+	defer func() {
+		err := ws.Close()
+		if err != nil {
+			log.Printf("Error: %v", err)
+		}
+
+		log.Printf("connection id %v closed", conn_num)
+		delete(connections, conn_num)
+	}()
 	io.Copy(ws, ws)
 }
 
+// Send a message to all clients every time a new client connects.
+func EmitServer(ws *websocket.Conn) {
+
+	conn_num := num_connections // will be used as an identifier for the connection.
+	connections[conn_num] = ws  // Save the connection object in a map.
+	num_connections++
+
+	log.Printf("connection id %v opened", conn_num)
+
+	defer func() { // On connection closed
+		err := ws.Close()
+		if err != nil {
+			log.Printf("Error: %v", err)
+		}
+
+		log.Printf("connection id %v closed", conn_num)
+		delete(connections, conn_num) // Remove connection from map.
+	}()
+
+	for _, val := range connections { // Write to all clients concurrently.
+		go val.Write([]byte("New client connected: ID " + strconv.FormatInt(int64(num_connections-1), 10)))
+	}
+
+	io.Copy(ioutil.Discard, ws) // Keep connection open.
+}
+
 func main() {
-	http.Handle("/echo", websocket.Handler(EchoServer))
+	connections = make(map[uint64]*websocket.Conn)
+
+	http.Handle("/echo-ws", websocket.Handler(EchoServer))
+	http.Handle("/emit-ws", websocket.Handler(EmitServer))
+
 	http.Handle("/", http.FileServer(http.Dir("webroot")))
 
-	fmt.Printf("Listening on: http://localhost:8080")
+	fmt.Printf("Listening on: http://localhost:8080\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- webroot/src/main.js
+++ webroot/src/main.js
@@ -1,11 +1,11 @@
-function WebSocketTest()
+function EchoTest()
 {
   if ("WebSocket" in window)
   {
 	console.log("WebSocket is supported by your Browser.");
 	
 	// open a web socket
-	var ws = new WebSocket("ws://localhost:8080/echo"); // Connect to a websocket server.
+	var ws = new WebSocket("ws://localhost:8080/echo-ws"); // Connect to a websocket server.
 	
 	$(window).on('beforeunload', function(){ // Defer socket.close() until page is closed or we browse away.
 	    ws.close();
@@ -37,6 +37,44 @@ function WebSocketTest()
   }
 }
 
+function EmitTest()
+{
+  if ("WebSocket" in window)
+  {
+	console.log("WebSocket is supported by your Browser.");
+	
+	// open a web socket
+	var ws = new WebSocket("ws://localhost:8080/emit-ws"); // Connect to a websocket server.
+	
+	$(window).on('beforeunload', function(){ // Defer socket.close() until page is closed or we browse away.
+	    ws.close();
+	});
+	
+	ws.onopen = function()
+	{
+
+	};
+	
+	ws.onmessage = function (evt) 
+	{ 
+		var received_msg = evt.data;
+		console.log("Message received: " + received_msg);
+	};
+	
+	ws.onclose = function()
+	{ 
+		// websocket is closed.
+		console.log("Connection is closed..."); 
+	};
+  }
+  else
+  {
+	// The browser doesn't support WebSocket
+	console.log("WebSocket not supported by your Browser. Please use a modern web browser.");
+  }
+}
+
 $(function() {	// Runs once the DOM is loaded.
-	WebSocketTest();
+	//EchoTest();
+	EmitTest();
 });
